a9669f2b3e94ed33b712bf4dc4117a95dcaeb410
mvillalva <maxi.villalva@gmail.com>
2022-09-02 14:05:47 -0300

    Clase 03

--- clase-03/js/app.js
+++ clase-03/js/app.js
@@ -1,13 +1,13 @@
 const html = `<li>
-                  <div class="d-flex mb-2">
-                    <iframe class="grow shadow" width="246" src="https://www.youtube-nocookie.com/embed/IDf-3ibyHeM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <div class="container">
-                      <strong>Música para el Trabajo - Efectividad y Productividad Radio</strong> <br>
-                      <small class="text-secondary">Chill Music Lab 160 <i class="fas fa-check-circle"></i> usuarios</small> <br>
-                      <small>Maximice su productividad con Music Lab, sea eficiente y trabaje de manera más inteligente, no más difícil. Permítete relajarte y calmar tu mente mientras escuchas estas melodías motivadora...</small>
-                    </div>
-                    </div>
-                  </li>`
+                <div class="d-flex mb-2">
+                  <iframe class="grow shadow" width="246" src="https://www.youtube-nocookie.com/embed/IDf-3ibyHeM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <div class="container">
+                    <strong>Música para el Trabajo - Efectividad y Productividad Radio</strong> <br>
+                    <small class="text-secondary">Chill Music Lab 160 <i class="fas fa-check-circle"></i> usuarios</small> <br>
+                    <small>Maximice su productividad con Music Lab, sea eficiente y trabaje de manera más inteligente, no más difícil. Permítete relajarte y calmar tu mente mientras escuchas estas melodías motivadora...</small>
+                  </div>
+                </div>
+              </li>`
 
 const mostrar_video = () => {
   let video = document.getElementById('video')
