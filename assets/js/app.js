16eca8359ad4e666dc8e56b5cd5e3f6c3fa4c6db
mvillalva <maxi.villalva@gmail.com>
2022-09-02 14:05:04 -0300

    Clase 03

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,23 @@ window.addEventListener('load', ()=>{
   setTimeout(()=>{
     body[0].classList.toggle('overflow-hidden')
   }, 600)
+  
+  let tooltip_icon = document.querySelectorAll('.tooltip-icon')
+  let tooltip_data = document.querySelectorAll('.tooltip-data')
+  
+  tooltip_icon.forEach(element => {        
+    element.addEventListener('mouseover', e => {
+      if (e.target.parentElement.classList.contains('tooltip-icon')) {
+        e.target.parentElement.nextElementSibling.classList.add('tooltip-show')
+        e.target.parentElement.nextElementSibling.firstElementChild.classList.add('tooltip-show')
+      }
+    })
+  })
+
+  tooltip_data.forEach(element => {
+    element.addEventListener('mouseleave', e => {
+      e.currentTarget.parentElement.classList.remove('tooltip-show')
+    })
+  })
+
 })
---
+++ clase-03/js/app.js
@@ -0,0 +1,32 @@
+const html = `<li>
+                  <div class="d-flex mb-2">
+                    <iframe class="grow shadow" width="246" src="https://www.youtube-nocookie.com/embed/IDf-3ibyHeM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="container">
+                      <strong>Música para el Trabajo - Efectividad y Productividad Radio</strong> <br>
+                      <small class="text-secondary">Chill Music Lab 160 <i class="fas fa-check-circle"></i> usuarios</small> <br>
+                      <small>Maximice su productividad con Music Lab, sea eficiente y trabaje de manera más inteligente, no más difícil. Permítete relajarte y calmar tu mente mientras escuchas estas melodías motivadora...</small>
+                    </div>
+                    </div>
+                  </li>`
+
+const mostrar_video = () => {
+  let video = document.getElementById('video')
+  let cantidad = document.getElementById('cantidad').value
+  video.innerHTML = ''
+  for (let index = 0; index < parseInt(cantidad); index++) {
+    video.innerHTML += html
+  }                
+}
+
+window.addEventListener('load', () => {
+  document.getElementById('mostrar').addEventListener('click', e => {
+    mostrar_video()
+  })
+  
+  document.getElementById('cantidad').addEventListener('keyup', e => {
+    if (e.key == 'Enter') {
+      mostrar_video()
+    }
+  })
+  
+})
