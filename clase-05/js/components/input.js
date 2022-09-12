13c1357defcae048812fbabf5a1650369706b61c
mvillalva <maxi.villalva@gmail.com>
2022-09-12 09:15:13 -0300

    Clase 05

--- clase-05/js/components/input.js
+++ clase-05/js/components/input.js
@@ -15,13 +15,28 @@ export default class Input {
     let input = document.getElementById(this.id)
     input.addEventListener('keyup', e => {
       let valor = e.target.value
+      let icon = e.target.nextElementSibling      
       if(valor.length > 3){
-        input.classList.add('ok')
         input.classList.remove('error')
+        icon.classList.remove('fa')
+        icon.classList.remove('fa-times-circle')
+        icon.classList.remove('text-danger')
+
+        input.classList.add('ok')
+        icon.classList.add('fas')
+        icon.classList.add('fa-check-circle')
+        icon.classList.add('text-success')
       }
       else {
         input.classList.remove('ok')
+        icon.classList.remove('fas')
+        icon.classList.remove('fa-check')
+        icon.classList.remove('text-success')
+
         input.classList.add('error')
+        icon.classList.add('fas')
+        icon.classList.add('fa-times-circle')
+        icon.classList.add('text-danger')
       }
     })
   }
@@ -31,14 +46,18 @@ export default class Input {
     if (this.label) {
       html += `<label ${this.name?'for="'+this.name+'"':''} class="${this.label_css}">${this.label}</label>`
     }
-    html += `<input type='${this.type}'
-                    ${this.id?'id="'+this.id+'"':''} 
-                    ${this.name?'name="'+this.name+'"':''} 
-                    ${this.value?'value="'+this.value+'"':''} 
-                    ${this.placeholder?'placeholder="'+this.placeholder+'"':''} 
-                    ${this.properties} 
-                    class="${this.css}"
-                />`
+    html += ` <div class="input-content">
+                <input type='${this.type}'
+                      ${this.id?'id="'+this.id+'"':''} 
+                      ${this.name?'name="'+this.name+'"':''} 
+                      ${this.value?'value="'+this.value+'"':''} 
+                      ${this.placeholder?'placeholder="'+this.placeholder+'"':''} 
+                      ${this.properties} 
+                      class="${this.css}"
+                />
+                <i class="input-icon"></i>
+              </div>
+            `
     return html
   }
 }
