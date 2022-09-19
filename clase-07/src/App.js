81e5cdc70b3749efd1a479492f6f757e7d157b74
mvillalva <maxi.villalva@gmail.com>
2022-09-19 18:23:51 -0300

    Clase 07 - Build

--- clase-07/src/App.js
+++ clase-07/src/App.js
@@ -15,18 +15,19 @@ function App() {
       <header className="App-header">
         <img src={'.'+logo} className="App-logo" alt="logo" />        
         <br></br>
-        <h1>Componente Button</h1>
+        <h1>Componente "Button"</h1>
         <Button label="Login"/>
         <br></br>
-        <h1>Componente Video</h1>
+        <h1>Componente "Video"</h1>
         <Video url={mp4} type="video/mp4"/>
         <br></br>
-        <h1>Componente Audio</h1>
+        <h1>Componente "Audio"</h1>
         <Audio url={mp3} type="audio/mpeg"/>
         <br></br>
-        <h1>Componente List</h1>
+        <h1>Componente "List"</h1>
         <List items={['Item 1', 'Item 2', 'Item 3', 'Item 4']}/>
         <br></br>
+        <h1>Componente "Input"</h1>
         <Input name="text" type="text" placeholder="Ingrese su nombre" label="Nombre"/>
         <Input name="email" type="email" placeholder="Ingrese su email" label="Email"/>
         <Input name="pass" type="password" placeholder="Ingrese su contraseña" label="Contraseña"/>
