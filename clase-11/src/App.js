982bdd3831d5d78949effb8802d003ad5ca576b1
mvillalva <maxi.villalva@gmail.com>
2022-10-03 08:52:54 -0300

    Clase 11 - Lista de imagenes

--- clase-11/src/App.js
+++ clase-11/src/App.js
@@ -1,24 +1,58 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Movies from "./movies/Movies";
+import Image from "./Image/Image";
 
-const myData = [
-    { framework: "Bootstrap" }, 
-    { framework: "Material" }, 
-    { framework: "Tailwind" }, 
-    { framework: "Next" }
+const images = [
+    {   index: 1,
+        src: "https://picsum.photos/id/1/300/200"
+    }, 
+    {   index: 2,
+        src: "https://picsum.photos/id/10/300/200"
+    },
+    {   index: 3,
+        src: "https://picsum.photos/id/21/300/200", 
+    },
+    {   index: 4,
+        src: "https://picsum.photos/id/30/300/200", 
+    },
+    {   index: 5,
+        src: "https://picsum.photos/id/41/300/200", 
+    },
+    {   index: 6,
+        src: "https://picsum.photos/id/50/300/200",
+    },
+    {   index: 7,
+        src: "https://picsum.photos/id/61/300/200", 
+    },
+    {   index: 8,
+        src: "https://picsum.photos/id/70/300/200", 
+    },
+    {   index: 9,
+        src: "https://picsum.photos/id/81/300/200", 
+    },
+    {   index: 10,
+        src: "https://picsum.photos/id/90/300/200", 
+    },
+    {   index: 11,
+        src: "https://picsum.photos/id/101/300/200", 
+    },
+    {   index: 12,
+        src: "https://picsum.photos/id/110/300/200"
+    }
 ];
 
-const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+function App() {    
+    const imageList = images.filter(src => {
+        if (src.index % 1 == 0)
+            return src
 
-function App() {
-    const filterResult = words.filter(word => word.length > 6);
-    const myDataList = myData.map((e) => <h2>{e.framework}</h2>);
+    }).map(e => <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3"><Image src={e.src} label={e.index}/></div>);
 
     return (
         <div className="App">
-            {myDataList}
-            <Movies data={filterResult} />
+            <h1>Lista de Imágenes</h1>
+            <div className="row w-100 text-center">
+                {imageList}
+            </div>
         </div>
     );
 }
---
+++ clase-11/src/Image/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Image(props) {
+   
+    return (
+        <div className="position-relative">
+            <h1 className="position-absolute p-3 text-warning">{props.label}</h1>
+            <img className="w-100 rounded-2 shadow" src={props.src} alt='Imagen' />
+        </div>
+    )
+}
